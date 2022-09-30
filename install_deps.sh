a85a41bcf58504c099e1765a6abb537cfa64bf82
strikingLoo <lucianostrika44@gmail.com>
2022-09-30 11:05:51 -0300

    initial commit. Run install_deps.sh, then make_images.py after setting the prompts

---
+++ install_deps.sh
@@ -0,0 +1,6 @@
+pip install --upgrade keras-cv
+conda install -c apple tensorflow-deps==2.9.0
+python -m pip install tensorflow-macos==2.9.0
+pip install numpy
+pip install matplotlib
+pip install tensorflow-datasets
---
+++ make_images.py
@@ -0,0 +1,24 @@
+import time
+from PIL import Image
+import keras_cv
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import sys 
+
+model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+
+def plot_images(images):
+    plt.figure(figsize=(20, 20))
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i + 1)
+        plt.imshow(images[i])
+        plt.axis("off")
+
+with open('prompts.txt') as f:
+	for prompt in f.read().split('\n'):
+		print(prompt)
+		images = model.text_to_image(prompt, batch_size=3)
+
+		for i, img in enumerate(images):
+		  im = Image.fromarray(img)
+		  im.save(f"{prompt[:240]}{i}.png")
